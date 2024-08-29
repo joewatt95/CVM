c1f3db01f5e6fe778c93956b766475fbc6270142
joewatt95 <joewatt95@u.nus.edu>
2024-08-29 15:06:17 +0800

    t

--- isabelle/Props/Props_Basic.thy
+++ isabelle/Props/Props_Basic.thy
@@ -13,16 +13,16 @@ abbreviation eps_del_approxs ("_ \<approx> \<langle> _ , _ \<rangle> _") where "
     \<P>(\<omega> in measure_pmf f. \<omega> \<in> {(1 - \<epsilon>) * x .. (1 + \<epsilon>) * x})
     \<ge> 1 - \<delta>"
 
-definition estimate_size :: "'a set \<Rightarrow> 'a set \<Rightarrow> nat \<Rightarrow> real pmf" where
+definition estimate_size :: "nat \<Rightarrow> 'a set \<Rightarrow> real pmf" where
   [simp] : "
-  estimate_size universe chi k \<equiv> (
+  estimate_size k chi \<equiv> (
     let
       estimate_size_with :: ('a \<Rightarrow> bool) \<Rightarrow> nat = \<lambda> keep_in_chi.
         let chi = Set.filter keep_in_chi chi
         in card chi * 2 ^ k;
 
       keep_in_chi :: ('a \<Rightarrow> bool) pmf =
-        Pi_pmf universe undefined <| \<lambda> _. bernoulli_pmf (1 / 2 ^ k)
+        Pi_pmf chi undefined <| \<lambda> _. bernoulli_pmf (1 / 2 ^ k)
 
     in map_pmf estimate_size_with keep_in_chi)"
 
@@ -30,14 +30,13 @@ lemma estimate_size_approx_correct :
   fixes
     \<epsilon> \<delta> threshold :: real and
     k :: nat and
-    universe chi :: "'a set"
+    chi :: "'a set"
   defines "chi_size :: real \<equiv> card chi"
   assumes "
-    finite universe" and "
-    chi \<subseteq> universe" and "
+    finite chi" and "
     k \<in> {log2 (chi_size / threshold) .. log2 chi_size}"
   shows "
-    (estimate_size universe chi k) \<approx>\<langle>\<epsilon>, \<delta>\<rangle> chi_size"
+    (estimate_size k chi) \<approx>\<langle>\<epsilon>, \<delta>\<rangle> chi_size"
 proof -
   show ?thesis sorry
 qed
