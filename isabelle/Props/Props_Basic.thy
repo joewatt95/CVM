01a25222b874a67f925e9d1a58d44d4e0865763f
Joe Watt <joewatt95@u.nus.edu>
2024-08-31 17:38:01 +0800

    doc

--- isabelle/Props/Props_Basic.thy
+++ isabelle/Props/Props_Basic.thy
@@ -54,10 +54,11 @@ lemma estimate_size_approx_correct :
     (map_pmf real <| estimate_size k chi) \<approx>\<langle>\<epsilon>, \<delta>\<rangle> (card chi)"
 proof -
   (*
-  Intuitively, the idea here is that we may assume wlog that chi is nonempty,
-  because if chi were empty, then `estimate_size_empty` is the key result that
+  First, we may assume wlog that chi is nonempty, because if chi were empty,
+  then `estimate_size_empty` is the key result that
   says the function is simply the 0 distribution, and so we always get the exact,
   correct solution when we sample from it.
+
   To get this to work, I (Joe) had to:
   1. Increase timeouts: sledgehammer[timeout = 60, preplay_timeout = 10]
   2. Explicitly pass in the key `estimate_size_empty` lemma, and `that`. 
@@ -76,15 +77,19 @@ proof -
     let ?binom_mean = "card chi * ?binom_prob"
 
     (*
-    The key observation here is that one can:
-    1. First use `binomial_pmf_altdef'` to rewrite a `binomial_pmf` into a
+    Next, we show that `?chi_size_est` is the same distribution as `?binom`,
+    modulo a factor of `2 ^ k`.
+    We do this because we want to bound the probability of the former by the
+    latter, and then apply Hoeffding's inequality for the binomial distribution.
+
+    The key observations and steps are:
+    1. We first use `binomial_pmf_altdef'` to rewrite a `binomial_pmf` into a
       `Pi_pmf` of bernoullis.
        This puts `?binom` into a similar form as `?chi_size_est`, on which we
        can perform more simplifications.
-    2. Then use Functor laws via `map_pmf_comp` to squish multiple chained
-       `map_pmf` down into a single one.
-    3. Finish the proof via a congruence property of `map_pmf` via
-       `map_pmf_cong` and routine simplifications.
+    2. Finish the proof via a congruence property of `map_pmf` via
+       `map_pmf_cong` and routine simplifications, using the functor laws
+       via `map_pmf_comp` to squish multiple `map_pmf` into one.
 
     Sledgehammer failed to prove this, despite my (Joe) attempts to augment
     it with all the various lemmas (which I had to manually identify).
@@ -97,6 +102,7 @@ proof -
                simp add: map_pmf_comp assms(3) Set.filter_def)
 
     (*
+    We bound the probability of `?chi_size_est` with that of `?binom`.
     Intuitively, the idea here is to:
     1. Use the monotonocity of `?chi_size_est` (viewed as a measure) with
        the fact that `x > y \<longrightarrow> x \<ge> y`.
@@ -117,10 +123,15 @@ proof -
       by (auto intro!: measure_pmf.finite_measure_mono)
 
     (*
+    We then transform the expression into a form that's more suitable for
+    the Hoeffding inequality `binomial_distribution.prob_abs_ge` in the HOL
+    probability lib.
+
     The idea here is that both probabilities are equal, because the arithmetic
     formula in the second probability is obtained by dividing that of the first
     by `2 ^ k`.
-    All tactics (including Sledgehammer) struggled with this and my (Joe)
+
+    All tactics (including sledgehammer) struggled with this and my (Joe)
     suspicion is that they weren't aware that:
     1. I wanted to divide by `2 ^ k` on both sides, and
     2. We can safely pull the division into the absolute value.
@@ -132,16 +143,18 @@ proof -
     proof -
       have * : "\<And> x y. \<bar>x\<bar> \<ge> y \<longleftrightarrow> \<bar>?binom_prob * x\<bar> \<ge> ?binom_prob * y"
         by (simp add: divide_le_cancel)
-      then show ?thesis by (subst *, simp add: diff_divide_distrib)
+      show ?thesis by (subst *, simp add: diff_divide_distrib)
     qed
 
     (*
-    Sledgehammer struggled hard to prove this.
+    Next, we apply Hoeffding's inequality for binomials.
+
+    It was difficult to get sledgehammer to prove this.
     I (Joe) needed to manually:
     1. Identify the appropriate Hoeffding inequality, namely
        `binomial_distribution.prob_abs_ge`, and explicitly pass it in. 
     2. Simplify the expressions to obtain a form for which the provers
-       Sledgehammer called could apply the above inequality.
+       sledgehammer called could apply the above inequality.
        This was tricky because simp was too eager, so that I had to first prove 
        an equality which undoes a simplification, and then run sledgehammer.
     3. Increase timeouts and limit max_facts:
@@ -155,6 +168,7 @@ proof -
         by (simp, metis \<open>0 < card chi\<close> assms(1) atLeastAtMost_iff binomial_distribution.intro divide_le_eq_1_pos divide_pos_pos less_eq_real_def mult_pos_pos of_nat_0_less_iff two_realpow_ge_one zero_le_divide_1_iff zero_less_numeral zero_less_power)
     qed
 
+    (* Finally, we do some simplifications and relaxation of the bound. *)
     also have "... = 2 * exp (-2 * \<epsilon> ^ 2 * card chi / 2 ^ (2 * k))"
       by (simp add: power2_eq_square power_even_eq)
 
