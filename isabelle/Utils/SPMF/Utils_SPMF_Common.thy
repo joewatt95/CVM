4ffe95fc9cfdd11846a6619b62ee88395a3fd947
Joe Watt <joewatt95@u.nus.edu>
2024-09-16 21:11:03 +0800

    wip

--- isabelle/Utils/SPMF/Utils_SPMF_Common.thy
+++ isabelle/Utils/SPMF/Utils_SPMF_Common.thy
@@ -22,17 +22,33 @@ definition kleisli_compose_right ::
   (infixr \<open><=<\<close> 50) where
   \<open>(f <=< g) \<equiv> g >=> f\<close>
 
-definition equiv_up_to_failure where
-  \<open>equiv_up_to_failure P p P' p' \<equiv>
-    rel_spmf (\<lambda> x x'. P x \<longleftrightarrow> P' x') p p'\<close>
+definition equiv_up_to_failure ::
+  \<open>('a \<Rightarrow> bool) \<Rightarrow> 'a spmf \<Rightarrow> ('b \<Rightarrow> bool) \<Rightarrow> 'b spmf \<Rightarrow> bool\<close>
+  (\<open>\<turnstile> \<lbrakk> _ \<rbrakk> _ \<simeq> \<lbrakk> _ \<rbrakk> _\<close>) where
+  \<open>\<turnstile> \<lbrakk> P \<rbrakk> p \<simeq> \<lbrakk> P' \<rbrakk> p' \<equiv> rel_spmf (\<lambda> x x'. P x \<longleftrightarrow> P' x') p p'\<close>
 
 lemma test :
   fixes p p' P P'
-  assumes \<open>equiv_up_to_failure P p P' p'\<close>
+  assumes \<open>\<turnstile> \<lbrakk>P\<rbrakk>p \<simeq> \<lbrakk>P'\<rbrakk>p'\<close>
+  defines
+    \<open>prob \<equiv> \<P>(x in measure_spmf p. P x)\<close> and
+    \<open>prob' \<equiv> \<P>(x' in measure_spmf p'. P' x')\<close>
   shows
     \<open>prob_fail p = prob_fail p'\<close> and
-    \<open>\<bar>\<P>(x in measure_spmf p. P x) - \<P>(x' in measure_spmf p'. P' x')\<bar> \<le> prob_fail p\<close>
+    \<open>\<bar>prob - prob'\<bar> \<le> prob_fail p\<close> and
+    \<open>prob \<le> prob' + prob_fail p\<close>
+proof -
+  show \<open>prob_fail p = prob_fail p'\<close>
+    by (metis assms(1) equiv_up_to_failure_def pmf_None_eq_weight_spmf prob_fail_def rel_spmf_weightD)
+  
+  then show \<open>\<bar>prob - prob'\<bar> \<le> prob_fail p\<close>
+    using assms
+    apply (auto simp add: equiv_up_to_failure_def prob_fail_def measure_measure_spmf_conv_measure_pmf)
+    sorry
 
-  sorry
+  then show \<open>prob \<le> prob' + prob_fail p\<close> by linarith
+
+qed
+
 
 end
