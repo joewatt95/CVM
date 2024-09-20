26a6fa6237a9aa1d5a5443c70e81bead6a8c3382
Joe Watt <joewatt95@u.nus.edu>
2024-09-20 17:16:48 +0800

    wip

--- isabelle/CVM.thy
+++ isabelle/CVM.thy
@@ -6,4 +6,48 @@ imports
 
 begin
 
+context basic_algo
+begin
+
+lemma test0 :
+  fixes xs k
+  defines
+    \<open>R final_state estimated_size' \<equiv>
+      k = state_k final_state
+        \<longrightarrow> (state_estimated_size final_state :: real) = estimated_size'\<close>
+  shows
+    \<open>rel_pmf R
+      (estimate_distinct_pmf xs)
+      (Final_Algo.estimate_distinct k <| set xs)\<close>
+
+  sorry
+
+lemma test1 :
+  fixes
+    P :: \<open>real \<Rightarrow> bool\<close>
+  shows
+    \<open>\<P>(state in estimate_distinct_pmf xs.
+        P (state_k state) \<and> k = state_k state)
+      = \<P>(estimated_size in Final_Algo.estimate_distinct k <| set xs.
+            P estimated_size)\<close>
+
+  apply simp
+  sorry
+
+find_theorems "measure_pmf.prob ?p ?A \<le> measure_pmf.prob ?q ?A"
+
+thm pmf.rel_mono
+
+lemma test2 :
+  fixes P :: \<open>real \<Rightarrow> bool\<close>
+  shows
+    \<open>\<P>(state in cond_pmf (estimate_distinct_pmf xs) {final_state. k = state_k final_state}.
+        P (state_k state))
+      \<le> \<P>(estimated_size in Final_Algo.estimate_distinct k (set xs). P estimated_size)\<close>
+
+  apply simp
+  sorry
+
+end
+
 end
