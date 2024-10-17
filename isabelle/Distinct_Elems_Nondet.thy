c2dce97c3763a3af173b8584925bf5fb1b5a07eb
joewatt95 <joewatt95@u.nus.edu>
2024-10-17 17:23:13 +0800

    wip

--- isabelle/Distinct_Elems_Nondet.thy
+++ isabelle/Distinct_Elems_Nondet.thy
@@ -41,7 +41,7 @@ lemma eager_step_1_inv:
       (run_reader (eager_step_1 xs i state) \<phi>)"
   using assms
   unfolding eager_step_1_def eager_state_inv_def nondet_alg_aux_def
-  apply (auto simp add: in_set_conv_nth run_reader_simps Let_def find_last_before_def)
+  apply (auto simp add: in_set_conv_nth run_reader_simps Let_def)
   apply (metis find_last_before_def find_last_before_self_eq semiring_norm(174))
   apply (meson less_SucI nth_take)
   sorry
@@ -54,8 +54,8 @@ lemma eager_step_2_inv:
       (run_reader (eager_step_2 xs i state) \<phi>)"
   using inv
   unfolding eager_step_2_def eager_state_inv_def nondet_alg_aux_def
-  apply (auto simp add: run_reader_simps Let_def  find_last_before_def)
-  by (metis add.commute find_last_before_def less_SucE plus_1_eq_Suc)
+  apply (auto simp add: run_reader_simps Let_def find_last_before_def)
+  by (metis less_SucE)
 
 lemma eager_step_inv:
   assumes i:"i < length xs"
