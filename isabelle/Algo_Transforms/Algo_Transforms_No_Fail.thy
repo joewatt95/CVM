aee2e150bb6f1826efbdca5d2af6367765fe8353
joewatt95 <joewatt95@u.nus.edu>
2024-11-19 11:26:06 +0800

    fix

--- isabelle/Algo_Transforms/Algo_Transforms_No_Fail.thy
+++ isabelle/Algo_Transforms/Algo_Transforms_No_Fail.thy
@@ -164,7 +164,7 @@ lemma estimate_distinct_ord_spmf_eq :
 
 (* Think of P as event that `estimate` is the wrong count *)
 theorem prob_estimate_distinct_fails_or_satisfies_le :
-  \<open>\<P>(estimate in estimate_distinct xs. estimate |> fail_or_fails_or_satisfies
+  \<open>\<P>(estimate in estimate_distinct xs. estimate |> fails_or_satisfies P)
   \<le> real (length xs) / 2 ^ threshold
     + \<P>(estimate in estimate_distinct_no_fail xs. P estimate)\<close>
   by (smt (verit, del_insts) Collect_cong estimate_distinct_ord_spmf_eq measure_spmf_spmf_of_pmf prob_fail_estimate_size_le prob_fails_or_satisfies_le_prob_fail_plus_prob prob_le_prob_of_ord_spmf_eq)
