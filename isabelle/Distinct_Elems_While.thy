c15fbcf509c01786f2ff57a0c43c9d7fab82e75e
Joe Watt <joewatt95@u.nus.edu>
2024-11-29 23:31:46 +0800

    typo

--- isabelle/Distinct_Elems_While.thy
+++ isabelle/Distinct_Elems_While.thy
@@ -109,7 +109,7 @@ proof -
 
     TODO: Try to derive a relational Hoare logic proof rule that provides a
     simpler API by abstracting away all this domain-theoretic fiddling.
-    For rereference, see ch5 of http://publications.rwth-aachen.de/record/814578/files/814578.pdf *)
+    For reference, see ch5 of http://publications.rwth-aachen.de/record/814578/files/814578.pdf *)
     apply (intro spmf.leq_antisym)
     subgoal for x'
       apply (induction arbitrary: flag x x' rule: loop_spmf.while_fixp_induct[where guard = \<open>\<lambda> (x, _). cond x\<close>])
