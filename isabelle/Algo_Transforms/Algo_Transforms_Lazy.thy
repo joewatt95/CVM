87f3cfd07b75176fce75e8e1acf16103f95bd5be
Joe Watt <joewatt95@u.nus.edu>
2024-12-13 21:41:40 +0800

    fix theory

--- isabelle/Algo_Transforms/Algo_Transforms_Lazy.thy
+++ isabelle/Algo_Transforms/Algo_Transforms_Lazy.thy
@@ -186,3 +186,5 @@ qed
 theorem estimate_distinct_no_fail_eq_lazy_algo :
   \<open>estimate_distinct_no_fail = lazy_algorithm >>> map_pmf compute_estimate\<close>
   unfolding estimate_distinct_no_fail_def run_steps_then_estimate_def lazy_algorithm_eq_run_step_no_fail ..
+
+end
