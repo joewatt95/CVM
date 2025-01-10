2249c336471f8b4696f222ba943e48ad24e28bf4
Joe Watt <joewatt95@u.nus.edu>
2025-01-10 21:45:45 +0800

    finish and tidy unbiasedness pf

--- isabelle/Distinct_Elems/Distinct_Elems_No_Fail_Unbiased.thy
+++ isabelle/Distinct_Elems/Distinct_Elems_No_Fail_Unbiased.thy
@@ -6,7 +6,7 @@ imports
 begin
 
 abbreviation (input)
-  \<open>aux \<equiv> \<lambda> x state. indicat_real (state_chi state) x * 2 ^ (state_k state)\<close>
+  \<open>aux \<equiv> \<lambda> f x state. indicat_real (f <| state_chi state) x * 2 ^ (state_k state)\<close>
 
 context
   fixes state :: \<open>('a, 'b) state_scheme pmf\<close>
@@ -19,9 +19,9 @@ lemma step_1_preserves_finite_support :
 
 lemma step_1_preserves_expectation_eq_1 :
   assumes
-    \<open>\<And> x. x \<in> S \<Longrightarrow> measure_pmf.expectation state (aux x) = 1\<close>
+    \<open>\<And> x. x \<in> S \<Longrightarrow> measure_pmf.expectation state (aux id x) = 1\<close>
     \<open>x \<in> S \<or> x = x'\<close>
-  shows \<open>measure_pmf.expectation (state \<bind> step_1_no_fail x') (aux x) = 1\<close>
+  shows \<open>measure_pmf.expectation (state \<bind> step_1_no_fail x') (aux id x) = 1\<close>
 proof -
   from assms have \<open>(x \<in> S \<and> x \<noteq> x') \<or> x = x'\<close> by blast
 
@@ -65,64 +65,16 @@ proof -
     by (auto simp add: state_finite_support step_2_no_fail_def Let_def)
 qed
 
-lemma
-  assumes
-    \<open>AE state in state. finite (state_chi state)\<close>
-    \<open>measure_pmf.expectation state (aux x) = 1\<close>
-  shows \<open>measure_pmf.expectation (state \<bind> step_2_no_fail) (aux x) = 1\<close>
-proof -
-  from state_finite_support assms show ?thesis
-    unfolding step_2_no_fail_def Let_def
-    apply (subst pmf_expectation_bind)
-    apply auto
-    apply (subst set_prod_pmf)
-    using not_less_iff_gr_or_eq threshold_pos apply fastforce
-    apply (subst finite_UN)
-    apply auto
-    apply (metis card.infinite card_UNIV_bool finite_PiE semiring_norm(143) threshold_pos)
-
-    apply (subst (asm) integral_measure_pmf)
-    apply (auto
-      simp flip: map_pmf_def
-      simp add:
-        if_distrib if_distribR sum.If_cases uminus_set_def fun_Compl_def not_less
-        Set.filter_def algebra_simps)
-
-    apply (simp add: indicator_def)
-
-    subgoal premises prems
-    proof -
-      thm prems
-
-      (* Restrict the summations to be taken over those `s \<in> set_pmf state`
-      where `x \<in> state_chi s`. This is ok because otherwise, `I(x \<in> X) = 0`.
-      Then simplify using the Lemma below. *)
-      thm expectation_Pi_pmf_slice[
-        where
-          I = \<open>state_chi _\<close> and
-          M = \<open>\<lblot>bernoulli_pmf <| 1 / 2\<rblot>\<close> and
-          f = \<open>\<lambda> b. of_bool (x \<in> state_chi _ \<and> b)\<close> and
-          d = undefined, simplified]
-
-      (* have
-        \<open>measure_pmf.expectation (prod_pmf (state_chi s) (\<lambda> _. bernoulli_pmf (1 / 2))) (\<lambda>xa. of_bool (x \<in> state_chi s \<and> xa x)) =
-        measure_pmf.expectation (bernoulli_pmf (1 / 2)) (\<lambda> b. of_bool (x \<in> state_chi s \<and> b))\<close>
-        sorry *)
-
-      show ?thesis sorry
-    qed
-    done
-qed
-
 lemma step_2_preserves_expectation_eq_1 :
-  assumes \<open>measure_pmf.expectation state (aux x) = 1\<close>
+  assumes
+    \<open>measure_pmf.expectation state (aux id x) = 1\<close>
   shows
-    \<open>measure_pmf.expectation (state \<bind> step_2_no_fail) (aux x) = 1\<close>
+    \<open>measure_pmf.expectation (state \<bind> step_2_no_fail) (aux id x) = 1\<close>
     (is \<open>?L = _\<close>)
 proof -
   have
     \<open>?L = (\<Sum> s \<in> set_pmf state.
-      pmf state s * measure_pmf.expectation (step_2_no_fail s) (aux x))\<close>
+      pmf state s * measure_pmf.expectation (step_2_no_fail s) (aux id x))\<close>
     proof -
       from assms threshold_pos not_le have \<open>\<turnstile>pmf
         \<lbrakk>\<lblot>True\<rblot>\<rbrakk> \<lblot>state\<rblot>
@@ -140,21 +92,57 @@ proof -
         by (fastforce intro!: finite_UN_I finite_PiE)+
 
      with state_finite_support show ?thesis
-        apply (subst pmf_expectation_bind)
-        by (auto
-          dest: Utils_PMF_Hoare.hoare_tripleE
-          simp add: step_2_no_fail_def Let_def)
+      apply (subst pmf_expectation_bind)
+      by (auto
+        dest: Utils_PMF_Hoare.hoare_tripleE
+        simp add: step_2_no_fail_def Let_def)
     qed
 
-  also from state_finite_support have
-    \<open>\<dots> = measure_pmf.expectation state (aux x)\<close>
-    unfolding step_2_no_fail_def Let_def
+  also from state_finite_support have \<open>\<dots> = (
+    \<Sum> s \<in> set_pmf state \<inter> {s. card (state_chi s) < threshold}.
+      pmf state s * aux id x s) + (
+    \<Sum> s \<in> set_pmf state \<inter> {s. card (state_chi s) \<ge> threshold \<and> x \<in> state_chi s}.
+      pmf state s *
+      measure_pmf.expectation
+        (prod_pmf (state_chi s) \<lblot>bernoulli_pmf <| 1 / 2\<rblot>)
+        (\<lambda> s'. 2 * aux (Set.filter s') x s))\<close>
+    (is \<open>_ = sum ?f (?A (<)) + sum _ ?B\<close>)
     apply (simp
       flip: map_pmf_def
       add:
-        integral_measure_pmf if_distrib if_distribR sum.If_cases
-        uminus_set_def fun_Compl_def not_less)
-    sorry
+        step_2_no_fail_def if_distrib if_distribR Let_def
+        sum.If_cases uminus_set_def fun_Compl_def not_less algebra_simps)
+    apply (intro sum.mono_neutral_cong_right)
+    by auto
+
+  also have \<open>\<dots> =
+    (\<Sum> s \<in> ?A (<). ?f s) + (\<Sum> s \<in> ?B.
+      pmf state s * 2 ^ (state_k s + 1) *
+      measure_pmf.expectation
+        (bernoulli_pmf <| 1 / 2)
+        (\<lambda> b. of_bool (x \<in> state_chi s \<and> b)))\<close>
+    proof -
+      from threshold_pos
+      have \<open>finite chi\<close> if \<open>card chi \<ge> threshold\<close> for chi :: \<open>'a set\<close>
+        using that leD by fastforce
+
+      then show ?thesis
+        unfolding add.left_cancel indicator_def
+        apply (intro sum.cong, blast)
+        apply (subst
+          expectation_Pi_pmf_slice[where
+            I = \<open>state_chi _\<close> and M = \<open>\<lblot>bernoulli_pmf <| 1 / 2\<rblot>\<close>, symmetric])
+        by (auto simp add: integrable_measure_pmf_finite)
+    qed
+
+  also from state_finite_support have \<open>\<dots> =
+    (\<Sum> s \<in> ?A (<). ?f s) + (\<Sum> s \<in> ?A (\<ge>). ?f s)\<close>
+    by (auto intro: sum.mono_neutral_cong_left simp add: indicator_def)
+
+  also from state_finite_support have \<open>\<dots> =
+    measure_pmf.expectation state (aux id x)\<close>
+    apply (subst sum.union_disjoint[symmetric])
+    by (auto intro: sum.cong simp add: integral_measure_pmf)
 
   finally show ?thesis using assms by simp
 qed
